8c6ef7cb601ef71c43f794ab9ef72d7b939aaee0
prtdh <98329971+prtdh@users.noreply.github.com>
2022-03-05 16:41:51 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,26 @@
 const User=require('../model/User')
 const  bcrypt=require('bcryptjs')
 const jwt=require('jsonwebtoken')
+const Joi=require('@hapi/joi')
+
+const registerSchema = Joi.object({
+    name: Joi.string().min(5).required(),
+    email:Joi.string().email().min(8).required(),
+    password:Joi.string().required().min(6)
+})
+
+const loginSchema = Joi.object({
+    email:Joi.string().email().min(8).required(),
+    password:Joi.string().required().min(6)
+})
 
 const createUser=async (req,res)=>{
+
+    // validation
+    const {error}  = registerSchema.validate(req.body);
+    if (error) {
+     return res.status(400).send(error.details[0].message)  
+    } 
 
     // check if email already exist
     const emailExist=await User.findOne({email:req.body.email})
@@ -18,11 +36,11 @@ const createUser=async (req,res)=>{
         })
     try {
         const savedUser=await user.save()
-        res.status(200).json(savedUser)
+        res.status(201).json(savedUser)
     } catch (error) {
 res.status(400).json({error})
     }
-}
+ }
 
 // login user
 
@@ -30,16 +48,20 @@ const loginUser=async (req,res)=>{
      // check if email already exist
     const user=await User.findOne({email:req.body.email})
     if (!user) {
-       return res.status(400).send("email doesn't exist")
+       return res.status(404).send("email doesn't exist")
     }
     const validPass=await bcrypt.compare(req.body.password,user.password)
     if (!validPass) {
-      return  res.status(400).send('Invalid Password')
+      return  res.status(401).send('Invalid Password')
     }
 
     //generate jwt token
-    const token =jwt.sign({_id:user._id},process.env.jwt_secret)
-    res.header('auth-token',token).send(`user logged in with token : "${token}"`)
+    const token =jwt.sign({_id:user._id},process.env.jwt_secret,{
+
+           expiresIn: '8h' // expires in 8 hours
+
+      })
+    res.status(202).header('auth-token',token).send(`user logged in with token : "${token}"`)
 }
 
 
